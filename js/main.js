60bdf5021a2c2b0d02d1418c4a338ab1ae9e7c7c
Amir Khan <webmasteramir1@gmail.com>
2022-08-14 03:01:35 +0530

    neo-save-changes

---
+++ js/main.js
@@ -0,0 +1,40 @@
+function getCurrentLocation() {
+  fetch("https://api.ipregistry.co/?key=tryout")
+    .then(function (response) {
+      if (response?.status === 200) {
+        const location = response?.json().location;
+        if (location?.console) {
+          setCountryInfoInLocalStorage(location.country.code);
+        }
+        return response?.json();
+      } else {
+        redirectUser("IN");
+      }
+    })
+    .then(function (err) {
+      console.log(err);
+      redirectUser("IN");
+    });
+}
+
+function setCountryInfoInLocalStorage(code) {
+  if (code) {
+    localStorage.setItem("c_code", code);
+    redirectUser(code);
+  }
+}
+
+function getCurrentLocationFromStorages() {
+  const currentLocation = localStorage.getItem("c_code");
+  redirectUser(currentLocation);
+}
+
+function redirectUser(code) {
+  if (code === "DE") {
+    window.location.href = "/home-de";
+  } else {
+    window.location.href = "/home";
+  }
+}
+
+getCurrentLocation();
