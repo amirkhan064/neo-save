4ba07ab6f7670e1b444aa97bd338cc7d6d75e10d
Amir Khan <webmasteramir1@gmail.com>
2022-08-17 01:25:18 +0530

    added mobile responsive changes for business

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function getCurrentLocation() {
-  fetch("https://api.ipregistry.co/?key=tryout")
+  fetch('https://api.ipregistry.co/?key=tryout')
     .then(function (response) {
       if (response?.status === 200) {
         const location = response?.json().location;
@@ -8,32 +8,32 @@ function getCurrentLocation() {
         }
         return response?.json();
       } else {
-        redirectUser("IN");
+        redirectUser('IN');
       }
     })
     .then(function (err) {
       console.log(err);
-      redirectUser("IN");
+      redirectUser('IN');
     });
 }
 
 function setCountryInfoInLocalStorage(code) {
   if (code) {
-    localStorage.setItem("c_code", code);
+    localStorage.setItem('c_code', code);
     redirectUser(code);
   }
 }
 
 function getCurrentLocationFromStorages() {
-  const currentLocation = localStorage.getItem("c_code");
+  const currentLocation = localStorage.getItem('c_code');
   redirectUser(currentLocation);
 }
 
 function redirectUser(code) {
-  if (code === "DE") {
-    window.location.href = "/home-de";
+  if (code === 'DE') {
+    window.location.href = '/home-de';
   } else {
-    window.location.href = "/home";
+    window.location.href = '/home';
   }
 }
 
